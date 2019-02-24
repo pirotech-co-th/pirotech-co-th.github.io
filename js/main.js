242e2e4759d3d19e1663c6d4c0f51d29d4224a60
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2019-02-24 16:00:21 +0200

    brand adjust, intro text, footer credentials

--- js/main.js
+++ js/main.js
@@ -1,28 +1,7 @@
 $(document).ready(function(){	
-
-	$('header div.link>a').on('click', function (evt) {
-		$(evt.target).animate({ width: 'hide' }, 'fast');
-		$(evt.target.nextElementSibling).animate({ width: 'show' });	
-	});
-	$('ul.sub-link li a.go-back').on( 'click', function (evt) {
-		$(evt.target).parent().parent().animate({ width: 'hide' }, 'fast');
-		$(evt.target).parent().parent().prev().animate({ width: 'show' });	
-	});	
-
-
-	// menu 2:
-	$('.link_2 .front a').on('click', (evt)=>{
-		evt.preventDefault();
-		$(evt.target).parent().parent().css({
-			transform:'rotateY(180deg)'
-		})
-	});
-	$('.link_2 .back a.go-back').on('click', (evt)=>{
-		evt.preventDefault();
-		$(evt.target).parent().parent().parent().parent().css({
-			transform:'rotateY(0deg)'
-		})
-	});
+    // footer get current year:
+    let currentYear = document.getElementById('year');
+    currentYear.innerHTML = (new Date()).getFullYear();
 
 });// end ready
 
---
+++ js/nav-menu.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){	
+
+	$('header div.link>a').on('click', function (evt) {
+		$(evt.target).animate({ width: 'hide' }, 'fast');
+		$(evt.target.nextElementSibling).animate({ width: 'show' });	
+	});
+	$('ul.sub-link li a.go-back').on( 'click', function (evt) {
+		$(evt.target).parent().parent().animate({ width: 'hide' }, 'fast');
+		$(evt.target).parent().parent().prev().animate({ width: 'show' });	
+	});	
+
+
+	// menu 2:
+	$('.link_2 .front a').on('click', (evt)=>{
+		evt.preventDefault();
+		$(evt.target).parent().parent().css({
+			transform:'rotateY(180deg)'
+		})
+	});
+	$('.link_2 .back a.go-back').on('click', (evt)=>{
+		evt.preventDefault();
+		$(evt.target).parent().parent().parent().parent().css({
+			transform:'rotateY(0deg)'
+		})
+	});
+
+});// end ready
+
+
